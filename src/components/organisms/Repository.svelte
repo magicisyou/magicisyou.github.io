2ae21ef8ecc59c6254938bbe144d1472a0f492b0
youaremagic <145214127+youaremagic@users.noreply.github.com>
2023-11-25 16:47:17 +0530

    added image to repository component, added details of gvader

--- src/components/organisms/Repository.svelte
+++ src/components/organisms/Repository.svelte
@@ -2,12 +2,20 @@
 	export let name = 'Name';
 	export let description = 'Description';
 	export let src = '';
+	export let image_url='';
 </script>
 
-<div>
-	<h3>{name}</h3>
-	<p>{description}</p>
-	<a href={src} target="_blank">See github repository</a>
+<div class=main_div>
+	<div class=details_div>
+		<h3>{name}</h3>
+		<p>{description}</p>
+		<a href={src} target="_blank">See github repository</a>
+	</div>
+	<div class=image_container>
+		{#if image_url != ''}
+			<img src={image_url} alt={name}>
+		{/if}
+	</div>
 </div>
 
 <style lang="scss" scoped>
@@ -19,13 +27,44 @@
 			color: #4069df;
 		}
 	}
-	div {
+	.main_div {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		align-items: flex-start;
-		gap: 20px;
+		justify-content: space-between;
 		background-color: $box-color;
 		padding: 30px;
 		border-radius: 5px;
+		height: 200px;
+	}
+	.details_div {
+		gap: 20px;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		width:70%;
+	}
+	.image_container {
+		width:25%;
+		height:90%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	img {
+		width:100%;
+		height:auto;
+		max-height: 100%;
+	}
+	@media(max-width:650px) {
+		.main_div {
+			flex-direction: column;
+			height:auto;
+			gap:20px;
+		}
+		.image_container {
+			width:100%;
+			height:auto;
+		}
 	}
 </style>
--- src/routes/repositories/+page.svelte
+++ src/routes/repositories/+page.svelte
@@ -5,7 +5,8 @@
 		{
 			name: 'Sudoku',
 			description: 'Sudoku built with svelte and tauri',
-			src: `${gh_base_link}sudoku`
+			src: `${gh_base_link}sudoku`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/sudoku/main/docs/screenshot.webp',
 		},
 		{
 			name: 'Termux Theme Changer',
@@ -16,24 +17,34 @@
 			name: 'Repo Lang Stat',
 			description:
 				'Small cli line tool written in rust to get the statistics of programming languages used in a github repository',
-			src: `${gh_base_link}repo-lang-stat`
+			src: `${gh_base_link}repo-lang-stat`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/repo-lang-stat/main/docs/screenshot.webp',
 		},
 		{
 			name: 'HCJ Lab',
 			description:
 				'Tauri app to write and previw HTML, CSS and Javascript code snippets instatntly',
-			src: `${gh_base_link}hcj-lab`
+			src: `${gh_base_link}hcj-lab`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/hcj-lab/main/docs/screenshot1.webp',
 		},
 		{
 			name: 'IP Fetch',
 			description:
 				'Get geographical details of an ip address and print the location on a world map in the terminal',
-			src: `${gh_base_link}ip-fetch`
+			src: `${gh_base_link}ip-fetch`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/ip-fetch/main/docs/screenshot.webp',
+		},
+		{
+			name: 'GVADER',
+			description: 'GUI for VADER sentiment analysis',
+			src: `${gh_base_link}gvader`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/gvader/main/docs/screenshot.webp',
 		},
 		{
 			name: 'Color Palette',
 			description: 'Tauri app to create personal color palette',
-			src: `${gh_base_link}color-palette`
+			src: `${gh_base_link}color-palette`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/color-palette/main/docs/screenshot.webp',
 		},
 		{
 			name: 'Color Game',
@@ -43,7 +54,8 @@
 		{
 			name: 'Game 2048',
 			description: '2048 game built with tauri',
-			src: `${gh_base_link}game-2048`
+			src: `${gh_base_link}game-2048`,
+			image_url:'https://raw.githubusercontent.com/youaremagic/game-2048/main/docs/screenshot.webp',
 		}
 	];
 </script>
